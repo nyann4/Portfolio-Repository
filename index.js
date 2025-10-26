4df386e33df9d606bc9fe8d1ca27e7ad4d20dc68
nyann4 <94633581+nyann4@users.noreply.github.com>
2025-10-26 16:48:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -41,18 +41,3 @@ window.addEventListener("scroll", () => {
     alterStyles(isBackToTopRendered);
   }
 });
-
-function ClipboardCopy() {
-  // Get the text field
-  var copyText = document.getElementById("myText");
-
-  // Select the text field
-  copyText.select();
-  copyText.setSelectionRange(0, 99999); // For mobile devices
-
-   // Copy the text inside the text field
-  navigator.clipboard.writeText(copyText.value);
-
-  // Alert the copied text
-  alert("Copied the text: " + copyText.value);
-}
